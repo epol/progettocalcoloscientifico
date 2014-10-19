eaf582c628993b619b20ca1e73e3964827e68dac
Enrico Polesel <enrico.polesel@sns.it>
2014-10-19 23:23:04 +0200

    both tik method without derivate, tsvd not working yet

--- reg.py
+++ reg.py
@@ -22,28 +22,42 @@ import numpy.linalg
 import scipy.optimize
 
 
-def regularize(U,s,V,filters,err):
-    residual = lambda mu : numpy.linalg.norm(numpy.dot(numpy.diag(filters(mu) - numpy.ones(10)) , U.T  ))
+def regularize(U,s,V,b,filters,filtersprime,err):
+    # U^T *b
+    UTb = numpy.dot( U.T, b)
+    residual = lambda mu : numpy.sqrt(numpy.linalg.norm( (numpy.diag(filters(mu) - numpy.ones(10)) * UTb  )) )
+    residualprime = lambda mu : numpy.sum((UTb **2) * filters(mu) * filtersprime(mu) ) / residual(mu)
     to_be_zero = lambda mu: residual(mu) - err
-    param = (to_be_zero,s[0])   #TODO: user fprime: the derivative of to_be_zero
+    param = scipy.optimize.newton(to_be_zero,s[numpy.argmin(s>0) -1])#,fprime=residualprime)   #TODO: user fprime: the derivative of to_be_zero
+    if (param < 0):
+        param = 0
+    x = numpy.dot ( numpy.dot ( V.T, numpy.diag( filters(param) / s ) ) , UTb )
+    return x
 
 def solve(A,b,err,method="newtik"):
     (n,m) = A.shape
     U, s, V = numpy.linalg.svd(A)
-    new_b = numpy.transpose(U) * b
-    dirty_x = V * ( s ** (-1) ) * new_b 
     if method.lower() == "newtik":
-        filters = lambda mu : s / ( s**2 + mu **2 )
-        
+        filters = lambda mu : numpy.where ( s < mu , s**2 / mu**2 , 1)
+        filtersprime = lambda mu : 2*numpy.where ( s < mu , -2 * s**2 / mu**3 , 0)
+        x= regularize(U,s,V,b,filters,filtersprime,err)
+        return x
     elif method.lower() == "tik":
-        pass
+        filters = lambda mu : s**2 / ( s**2 + mu **2 )
+        filtersprime = lambda mu : - 2* mu * s**2 / ( ( s**2 + mu **2 ) **2 )
+        x= regularize(U,s,V,b,filters,filtersprime,err)
+        return x
     elif method.lower() == "tsvd":
         summer = numpy.zeros((m,m))
         summer [ numpy.tril_indices(m) ] = 1
-        solutions = summer*dirty_x
-        trigger = solutions < err 
-        alpha = numpy.argmax( trigger )
-        x = solutions (alpha)
+        UTb = numpy.dot(numpy.transpose(U) , b)
+        dirty_x_newbase = numpy.dot(numpy.diag( s ** (-1) ) , UTb)
+        solutions_newbase = numpy.dot(summer,dirty_x_newbase)
+        trigger = solutions_newbase < err 
+        alpha = numpy.argmin( trigger ) -1
+        x_newbase = dirty_x_newbase
+        x_newbase [ alpha+1: ] = 0
+        x = numpy.dot( V.T , x_newbase)
         return x
     else:
         raise Exception ("Unknow method "+method)
