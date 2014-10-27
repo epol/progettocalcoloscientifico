b4154efe6175334ab9a9bfa71d6e3b9f62179fdd
Enrico Polesel <enrico.polesel@sns.it>
2014-10-27 10:02:37 +0100

    meno debug più commenti

--- reg.py
+++ reg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2014 - Enrico Polesel
@@ -21,46 +21,25 @@ import numpy
 import numpy.linalg
 import scipy.optimize
 
-import pylab as pl
+#import pylab as pl
 
-
-#def singular_bin_search(to_be_zero,s):
-    ## we assume that residual is increasing in mu
-    #(n,) = s.shape
-    #first = 0
-    #last = n
-    #while last -first > 1:
-        #mid = first + (last -first) /2
-        #if to_be_zero((s[mid])**(-1)) > 0:
-            #last = mid
-        #else:
-            #first = mid
-    ##print ("bin param "+str((s[last-1])**(-1)))
-    #return (s[last-1])**(-1)
 
 def find_param(UTb, s, filters, filtersprime,err):
     residual = lambda mu : numpy.sum( numpy.dot(numpy.diag(filters(mu) - numpy.ones(s.size)) , UTb  ) **2)
     residualprime = lambda mu : 2*numpy.sum((UTb **2) * (filters(mu)-numpy.ones(s.size)) * filtersprime(mu) )
-    #mus = numpy.logspace(-10,1,100)
-    #residuals=numpy.zeros(shape=mus.shape)
-    #print residual(0)
-    #for i in range(0,100):
-    #    residuals[i] = residual(mus[i])
-    #pl.plot(mus,residuals)
-    #pl.show()
     to_be_zero = lambda mu: residual(mu) - err**2
     
     # newton zero search
     oldt = -1.
     t = 1e-4
     k=0 # iteration counter
-    #while ( abs(t - oldt) > 1e-4*t):
     while ( ( abs(to_be_zero(t)) / err **2 > 1e-5 ) and k<200):
         oldt = t
         t = t - to_be_zero(t) / residualprime(t)
         if (t < 0):
             # because filters are not defined for t<0
             t = oldt /2
+        # OLD debug: plot the zero search
         #mu = numpy.linspace (0,1.2*max(oldt,t) , 100)
         #dehs  = numpy.zeros(100)
         #for i in range (100):
@@ -73,43 +52,57 @@ def find_param(UTb, s, filters, filtersprime,err):
         #pl.show()
         k = k+1
     param = t
-    print ( "Discrepancy: " + str(to_be_zero(param))) 
     #param = scipy.optimize.newton(to_be_zero,singular_bin_search(to_be_zero,s))#,fprime=residualprime)   #TODO: user fprime: the derivative of to_be_zero
     #param = singular_bin_search(to_be_zero,s)
-    print ('param '+str(param))
     return param
 
 def solve(A,b,err,method="newtik"):
     (n,m) = A.shape
+    # SVD of A. WARNING:  A = U * diag(s) * V   (no V.T)
     U, s, V = numpy.linalg.svd(A)
+    
     if method.lower() == "newtik":
+        # new method filters
         filters = lambda mu : numpy.where ( s > mu , 1., s**2 /( mu**2 ))
         filtersprime = lambda mu : numpy.where ( s > mu , 0., -2. * s**2 / mu**3 )
+        # standard tikhonov filters
         filtersTIK = lambda mu : s**2 / ( s**2 + mu **2 )
-        filtersprimeTIK = lambda mu : - 2* mu * s**2 / ( ( s**2 + mu **2 ) **2 )        
+        filtersprimeTIK = lambda mu : - 2* mu * s**2 / ( ( s**2 + mu **2 ) **2 )
+        # U^T *b
         UTb = numpy.dot( U.T, b)
+        # curiosity: how much the parameter from discrepancy applied to this filters
         param = find_param(UTb,s,filters,filtersprime,err)
+        # now compute the regularization parameter using the standard tikhonov filters
         paramTIK = find_param(UTb,s,filtersTIK,filtersprimeTIK,err)
-        param = min(param,paramTIK)
+        param = min(param,paramTIK) # paramTIK should always be the min
+        # the solution
         x = numpy.dot ( V.T, numpy.dot( numpy.diag( filters(param) / s ) , UTb ) )
         return x
     elif method.lower() == "tik":
+        # standard tikhonov filters
         filters = lambda mu : s**2 / ( s**2 + mu **2 )
         filtersprime = lambda mu : - 2* mu * s**2 / ( ( s**2 + mu **2 ) **2 )
+        # U^T *b
         UTb = numpy.dot( U.T, b)
+        # use newton to find a parameter using discrepancy principle
         param = find_param(UTb,s,filters,filtersprime,err)
+        # the solution
         x = numpy.dot ( V.T, numpy.dot( numpy.diag( filters(param) / s ) , UTb ) )
         return x
     elif method.lower() == "tsvd":
+        # (summer * v )[i] = sum _i ^n v[i]
         summer = numpy.zeros((m,m))
         summer [ numpy.triu_indices(m,1) ] = 1
+        # U^T *b
         UTb = numpy.dot(numpy.transpose(U) , b)
         residual = numpy.dot( summer , UTb**2 )
+        # use discrepancy principle
         alpha = numpy.argmax (residual < (err)**2)
+        # truncated singual value vector
         ts = s
-        ts [ alpha +1: ] = numpy.inf # truncated s
+        ts [ alpha +1: ] = numpy.inf # because it's easier to use
+        # the solution
         x = numpy.dot( V.T , numpy.dot(numpy.diag( ts ** (-1) ) , UTb) )
-        print ("Discrepancy: "+str(numpy.sum( (numpy.dot(A,x) -b)**2 ) - err**2))
         return x
     else:
         raise Exception ("Unknow method "+method)
